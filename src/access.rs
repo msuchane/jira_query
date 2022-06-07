3a95c2f2e774ef2f5293a22c8cd2dd51e9202d80
Marek Suchánek <msuchane@redhat.com>
2022-06-07 17:36:41 +0200

    Rename IDs to keys to match Jira terms. Add doc comments

--- src/access.rs
+++ src/access.rs
@@ -1,4 +1,4 @@
-// API documentation:
+// Jira API documentation:
 // * https://docs.atlassian.com/software/jira/docs/api/REST/latest/
 // * https://docs.atlassian.com/jira-software/REST/latest/
 
@@ -12,25 +12,27 @@ use crate::issue_model::{Issue, JqlResults};
 // This string comes directly after the host in the URL.
 const REST_PREFIX: &str = "rest/api/2";
 
+/// Configuration and credentials to access a Jira instance.
 pub struct JiraInstance {
     pub host: String,
     pub auth: Auth,
 }
 
+/// The authentication method used to contact Jira.
 pub enum Auth {
     Anonymous,
     ApiKey(String),
 }
 
-// API call with one &str parameter (e.g. "https://issues.redhat.com/rest/api/2/issue/RHELPLAN-12345")
+/// API call with one &str parameter (e.g. "https://issues.redhat.com/rest/api/2/issue/RHELPLAN-12345")
 impl RestPath<&str> for Issue {
     fn get_path(param: &str) -> Result<String, Error> {
         Ok(format!("{}/issue/{}", REST_PREFIX, param))
     }
 }
 
-// API call with several &str parameters representing the IDs of issues.
 // TODO: Make this generic over &[&str] and &[String].
+/// API call with several &str parameters representing the IDs of issues.
 impl RestPath<&[&str]> for JqlResults {
     fn get_path(params: &[&str]) -> Result<String, Error> {
         Ok(format!(
@@ -54,22 +56,27 @@ impl JiraInstance {
         Ok(client)
     }
 
-    pub fn issue(&self, id: &str) -> Result<Issue, Error> {
+    // This method uses a separate implementation from `issues` because Jira provides a way
+    // to request a single ticket specifically. That conveniently handles error cases
+    // where no tickets might match, or more than one might.
+    /// Access a single issue by its key.
+    pub fn issue(&self, key: &str) -> Result<Issue, Error> {
         let client = self.client()?;
 
         // Gets a bug by ID and deserializes the JSON to data variable
-        let data: RestResponse<Issue> = client.get(id)?;
+        let data: RestResponse<Issue> = client.get(key)?;
         let issue = data.into_inner();
         debug!("{:#?}", issue);
 
         Ok(issue)
     }
 
-    pub fn issues(&self, ids: &[&str]) -> Result<Vec<Issue>, Error> {
+    /// Access several issues by their keys.
+    pub fn issues(&self, keys: &[&str]) -> Result<Vec<Issue>, Error> {
         let client = self.client()?;
 
         // Gets a bug by ID and deserializes the JSON to data variable
-        let data: RestResponse<JqlResults> = client.get(ids)?;
+        let data: RestResponse<JqlResults> = client.get(keys)?;
         let results = data.into_inner();
         debug!("{:#?}", results);
 
