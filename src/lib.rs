fc6a65f984735f4e16f535952c0dce9054dba7ef
Marek Suchánek <msuchane@redhat.com>
2022-06-07 17:25:35 +0200

    Publicly export all issue fields

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,9 @@ mod access;
 mod issue_model;
 
 pub use access::{Auth, JiraInstance};
-pub use issue_model::{Issue, JqlResults};
+pub use issue_model::{
+    AvatarUrls, Comment, Comments, Component, CondensedFields, CondensedIssue, Fields, Issue,
+    IssueLink, IssueLinkType, IssueType, LinkedIssue, LinkedIssueFields, Priority, Progress,
+    Project, ProjectCategory, Resolution, Status, StatusCategory, User, Version, Visibility, Votes,
+    Watches,
+};
