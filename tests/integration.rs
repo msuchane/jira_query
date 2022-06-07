01b488d2630ece9d94d43233ef307113431494ce
Marek Suchánek <msuchane@redhat.com>
2022-06-07 16:51:07 +0200

    Extend the integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -9,9 +9,11 @@ fn rh_jira() -> JiraInstance {
     }
 }
 
-/// Try accessing a public issue
+/// Try accessing several public issues to test the client and the deserialization.
 #[test]
-fn access_issue() {
+fn access_issues() {
     let instance = rh_jira();
-    let _issue = instance.issue("CS-1113").unwrap();
+    let _issue1 = instance.issue("CS-1113").unwrap();
+    let _issue2 = instance.issue("CS-1111").unwrap();
+    let _issues = instance.issues(&["CS-1086", "CS-1084"]);
 }
